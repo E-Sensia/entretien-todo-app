f85f7c325ae03980cc99bfc93a2cfa9c7761ea81
Giodall <giovanni.dallava@gmail.com>
2025-10-07 14:00:43 +0200

    feat: tasks as state

--- src/adapters/taskRepo/inMemTaskRepo.ts
+++ src/adapters/taskRepo/inMemTaskRepo.ts
@@ -2,11 +2,17 @@ import { Task, TaskRepository } from "@/core/models";
 
 
 export class InMemTaskRepo implements TaskRepository {
-    async getTasks(): Promise<Task[]> {
-        return [
+    private tasks: Task[] = [];
+
+    constructor() {
+        this.tasks = [
             { checked: true, txt: "Display a list of all task in the state" },
             { checked: false, txt: "Display a checkbox to mark the task" },
             { checked: false, txt: "Display an input and a button to add a task" },
-        ]
+        ];
+    }
+
+    async getTasks(): Promise<Task[]> {
+        return this.tasks;
     };
 };
